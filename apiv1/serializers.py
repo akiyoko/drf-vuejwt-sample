ae5a92384d6ab9bb924fb758ca283aefdfad436a
akiyoko <akiyoko@users.noreply.github.com>
2019-03-17 19:21:00 +0900

    Django fix 1

---
+++ apiv1/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from shop.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'price')
+
+
+class BookListSerializer(serializers.ListSerializer):
+    child = BookSerializer()
